2d0a0743d8ef22388b014742343d69fb09098ed0
caparoonie505 <caparoonie505@gmail.com>
2018-09-18 06:19:02 -0500

    Implemented rudimentary placeholders for individual alarms

---
+++ app/src/main/java/collinpotter/timeapplication/RecyclerViewAdapter.java
@@ -0,0 +1,87 @@
+package collinpotter.timeapplication;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private ArrayList<String> alarmTimes = new ArrayList<>();
+    private ArrayList<String> alarmNames = new ArrayList<>();
+    private ArrayList<String> alarmPeriods = new ArrayList<>();
+    private ArrayList<String> alarmDays = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(ArrayList<String> times, ArrayList<String> names, ArrayList<String> periods, ArrayList<String> days, Context context) {
+        this.alarmTimes = times;
+        this.alarmNames = names;
+        this.alarmPeriods = periods;
+        this.alarmDays = days;
+        this.mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_alarmlistitem,parent,false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called");
+
+        holder.alarmTime.setText(alarmTimes.get(position));
+        holder.alarmName.setText(alarmNames.get(position));
+        holder.alarmPeriod.setText(alarmPeriods.get(position));
+        holder.alarmDay.setText(alarmDays.get(position));
+
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: clicked on: " + alarmNames.get(position));
+
+                Toast.makeText(mContext,alarmNames.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return alarmNames.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView alarmTime;
+        TextView alarmName;
+        TextView alarmPeriod;
+        TextView alarmDay;
+        ConstraintLayout parentLayout;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            alarmTime = itemView.findViewById(R.id.alarm_item_time);
+            alarmName = itemView.findViewById(R.id.alarm_item_text);
+            alarmPeriod = itemView.findViewById(R.id.timePeriod);
+            alarmDay = itemView.findViewById(R.id.timeDays);
+            parentLayout = itemView.findViewById(R.id.alarm_parent_layout);
+        }
+    }
+}
--- app/src/main/java/collinpotter/timeapplication/fragmentAlarm.java
+++ app/src/main/java/collinpotter/timeapplication/fragmentAlarm.java
@@ -3,6 +3,8 @@ package collinpotter.timeapplication;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,10 +13,16 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class fragmentAlarm extends Fragment {
 
     private static final String TAG = "fragmentAlarm";
 
+    private ArrayList<String> mTime = new ArrayList<>();
+    private ArrayList<String> mName = new ArrayList<>();
+    private ArrayList<String> mPeriod = new ArrayList<>();
+    private ArrayList<String> mDays = new ArrayList<>();
 
     @Nullable
     @Override
@@ -22,6 +30,32 @@ public class fragmentAlarm extends Fragment {
         View view = inflater.inflate(R.layout.fragment_alarm,container,false);
         Log.d(TAG,"onCreateView: started.");
 
+        //First item
+        mTime.add("3:00");
+        mName.add("School");
+        mPeriod.add("PM");
+        mDays.add("S   M   T   W   T   F   S");
+        //Second item
+        mTime.add("4:00");
+        mName.add("Wake Up");
+        mPeriod.add("AM");
+        mDays.add("S   M   T   W   T   F   S");
+        //Third item
+        mTime.add("12:00");
+        mName.add("Lunch Time!");
+        mPeriod.add("PM");
+        mDays.add("S   M   T   W   T   F   S");
+        initRecyclerView(view);
+
         return view;
     }
+
+    private void initRecyclerView(View view){
+        Log.d(TAG, "initRecyclerView: init RecyclerView");
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerv_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(mTime,mName,mPeriod,mDays,this.getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+    }
+
 }
